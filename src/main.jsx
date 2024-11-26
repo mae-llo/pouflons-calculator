2cd2ba3957dcf5d4004a2044877effbbf5e5857c
mae-llo <maeganlloyd15@gmail.com>
2024-11-26 14:35:46 -0500

    Fix import

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Calculator from './Calculator.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import Calculator from './Calculator.tsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Calculator />
   </StrictMode>,
-)
+);
